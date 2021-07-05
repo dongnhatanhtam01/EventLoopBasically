a1c04393ba8d2db8a011ba251af99b505459b871
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2021-07-06 02:08:16 +0700

    các callback của nextTick chạy asap sau mỗi phase của el

--- event-loop.js
+++ event-loop.js
@@ -9,6 +9,9 @@ fs.readFile('text.txt', ()=>{
   setImmediate(()=>console.log("Immediately 2 finished"));
 
   // căng phết vì libuv hoạt động ranh giới với JS
+  /** lưu ý là nextTịck() chạy sau mỗi phase asap
+   * mà nãy chạy phase timer để xếp hàng đợi cho 2 thằng timer rồi
+   * nên nextTịck() chạy trước setImmidiate trong ca này */ 
   process.nextTick(()=>console.log("Process.nextTick done!"));
 })
 console.log("Hello from the top-level code");
