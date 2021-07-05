3bcfb4e59ffea4edc0599739d336e4a1b8c167e8
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2021-07-06 02:16:04 +0700

    thử nghiệm chạy 4 luồng threads trong concept libuv c++ và thời gian trả ra gần như nhau

--- event-loop.js
+++ event-loop.js
@@ -1,17 +1,36 @@
 const fs = require('fs');
+const crypto = require('crypto');
+// TODO: phụ trợ đếm giờ operation
+const start = Date.now();
 
-setTimeout(()=>console.log("TImer 1 finish!"),0);
-setImmediate(()=>console.log("Immididate 1 finish!"));
-fs.readFile('text.txt', ()=>{
+setTimeout(() => console.log("TImer 1 finish!"), 0);
+setImmediate(() => console.log("Immididate 1 finish!"));
+fs.readFile('text.txt', () => {
   console.log('I/O finished');
-  setTimeout(()=>console.log("Timeout 2 finished!"),0);
-  setTimeout(()=>console.log("Timeout 3 finished!"),3000);
-  setImmediate(()=>console.log("Immediately 2 finished"));
+  setTimeout(() => console.log("Timeout 2 finished!"), 0);
+  setTimeout(() => console.log("Timeout 3 finished!"), 3000);
+  setImmediate(() => console.log("Immediately 2 finished"));
 
   // căng phết vì libuv hoạt động ranh giới với JS
   /** lưu ý là nextTịck() chạy sau mỗi phase asap
    * mà nãy chạy phase timer để xếp hàng đợi cho 2 thằng timer rồi
-   * nên nextTịck() chạy trước setImmidiate trong ca này */ 
-  process.nextTick(()=>console.log("Process.nextTick done!"));
+   * nên nextTịck() chạy trước setImmidiate trong ca này */
+  process.nextTick(() => console.log("Process.nextTick done!"));
+
+  /** microTaskQueue thử nghiệm chạy 4 luồng libuv */
+  // thread #1
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password encrypted");
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password encrypted");
+  })
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password encrypted");
+  })
+  // thread #4
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, "Password encrypted");
+  })
 })
 console.log("Hello from the top-level code");
