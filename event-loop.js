85dee7b9ec9af5bd44af356abf3d78d29d417b1f
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2021-07-06 02:03:48 +0700

    wtf with this Tasks in nextTickQueue holds the callbacks invoked by using the api process.nextTick() They are called as soon as possible, whenever the boundary between C/C++ and JavaScript is crossed

--- event-loop.js
+++ event-loop.js
@@ -7,5 +7,8 @@ fs.readFile('text.txt', ()=>{
   setTimeout(()=>console.log("Timeout 2 finished!"),0);
   setTimeout(()=>console.log("Timeout 3 finished!"),3000);
   setImmediate(()=>console.log("Immediately 2 finished"));
+
+  // căng phết vì libuv hoạt động ranh giới với JS
+  process.nextTick(()=>console.log("Process.nextTick done!"));
 })
 console.log("Hello from the top-level code");
