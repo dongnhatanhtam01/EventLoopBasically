0c258e5fba6d52ea7b530c5cd68306a633f63d78
dongnhatanhtam01 <dongnhatanhtam01@gmail.com>
2021-07-07 00:33:49 +0700

    ôn tập 1 chút

--- event-loop.js
+++ event-loop.js
@@ -7,23 +7,22 @@ const start = Date.now();
 
 // tăng kích thước thread pool bằng cách set cứng 1
 // có nghĩa ép chạy 1 luồng
-process.env.UV_THREADPOOL_SIZE=2;
+process.env.UV_THREADPOOL_SIZE=1;
 
 setTimeout(() => console.log("TImer 1 finish!"), 0);
 setImmediate(() => console.log("Immididate 1 finish!"));
 fs.readFile('text.txt', () => {
   console.log('I/O finished');
   setTimeout(() => console.log("Timeout 2 finished!"), 0);
-  setTimeout(() => console.log("Timeout 3 finished!"), 3000);
   setImmediate(() => console.log("Immediately 2 finished"));
+  setTimeout(() => console.log("Timeout 3 finished!"), 3000);
 
   // căng phết vì libuv hoạt động ranh giới với JS
   /** lưu ý là nextTịck() chạy sau mỗi phase asap
    * mà nãy chạy phase timer để xếp hàng đợi cho 2 thằng timer rồi
-   * nên nextTịck() chạy trước setImmidiate trong ca này */
+   * nên nextTick() chạy trước setImmidiate trong ca này */
   process.nextTick(() => console.log("Process.nextTick done!"));
 
- 
   // synchro
   crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
   console.log(Date.now() - start, "Password encrypted");
